636a15b440b6e48021533741076ec7573422cb2b
Mythicalurbanssss <101818698+Mythicalurbanssss@users.noreply.github.com>
2022-03-17 12:05:33 -0400

    Create Robloxscript.lua

---
+++ Robloxscript.lua
@@ -0,0 +1,218 @@
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = "ATTENTION EVERYONE I AM HACKING",
+    [2] = "All",
+}
+ 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
